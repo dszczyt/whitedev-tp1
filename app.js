d35c4a188527c6b350db1b7583214503a9832d81
admin <admin@iMac-16.local>
2016-05-13 11:07:40 +0200

    add user through form in table

--- app.js
+++ app.js
@@ -2,34 +2,68 @@
   "use strict";
 
   angular.module('tp1', [
-    //'ui.router',
+    'ui.router',
     'restangular'
   ])
+  .config(function(RestangularProvider) {
+    RestangularProvider.setBaseUrl("http://localhost:3000");
+  })
+  .config(function($stateProvider) {
+    $stateProvider
+    .state(
+      "users",
+      {
+        url: "/users",
+        template: "" +
+          "<users users=\"vm.users\"></users>" +
+          "<a href=\"javascript:void(0);\" ui-sref=\".bidon\">lien vers l'état bidon</a>" +
+          "<ui-view/>",
+        resolve: {
+          users: function(Restangular) {
+            return Restangular.all("users").getList();
+          }
+        },
+        controller: function(users) {
+          this.users = users;
+        },
+        controllerAs: 'vm'
+      }
+    )
+    .state(
+      "users.bidon",
+      {
+        template: "<b>Ceci est l'état bidon</b> <a href=\"javascript:void(0);\" ui-sref=\"^\">retour à l'état parent</a>"
+      }
+    );
+  })
   .directive("users", function() {
     return {
       restrict: 'E',
-      scope: true,
+      scope: {
+        users: "="
+      },
       templateUrl: "/templates/users.html",
       controllerAs: "vm",
       bindToController: true,
-      controller: function(Restangular) {
-        Restangular.allUrl("users", "http://localhost:3000/users")
-        .getList()
-        .then((res) => {
-          console.log(arguments);
-          this.users = res;
-        });
+      controller: function(Restangular, $state) {
+        this.saveNewUser = function(userName) {
+          Restangular.all("users").post({
+            name: userName
+          }).then(function() {
+            $state.reload();
+          });
+        };
       }
     };
   }).directive("user", function() {
     return {
-      restrict: 'E',
+      restrict: 'EA',
       scope: {
-        name: "=",
-        id: "="
+        user: "=",
       },
       controllerAs: "vm",
       bindToController: true,
+      //replace: true,
       templateUrl: "/templates/user.html",
       controller: function() {
 
