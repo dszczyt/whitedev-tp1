0bd9b3e8abaab2bb4ae17492963e33574e88b114
admin <admin@iMac-16.local>
2016-05-13 14:11:01 +0200

    display user detils

--- app.js
+++ app.js
@@ -30,6 +30,22 @@
       }
     )
     .state(
+      "user",
+      {
+        url: "/users/:id",
+        template: "<div user=\"vm.user\" mode=\"details\"></div>",
+        resolve: {
+          user: function(Restangular, $stateParams) {
+            return Restangular.one("users", $stateParams.id).get();
+          }
+        },
+        controller: function(user) {
+          this.user = user;
+        },
+        controllerAs: 'vm'
+      }
+    )
+    .state(
       "users.bidon",
       {
         template: "<b>Ceci est l'état bidon</b> <a href=\"javascript:void(0);\" ui-sref=\"^\">retour à l'état parent</a>"
@@ -46,11 +62,18 @@
       controllerAs: "vm",
       bindToController: true,
       controller: function(Restangular, $state) {
+        this.userElements = [];
         this.saveNewUser = function(userName) {
           Restangular.all("users").post({
             name: userName
           }).then(function() {
             $state.reload();
+          });
+        };
+
+        this.test = function() {
+          this.userElements.forEach(function(user) {
+            console.log(user);
           });
         };
       }
@@ -60,13 +83,28 @@
       restrict: 'EA',
       scope: {
         user: "=",
+        mode: "@"
       },
+      require: '?^users',
       controllerAs: "vm",
       bindToController: true,
       //replace: true,
-      templateUrl: "/templates/user.html",
+      templateUrl: function(element, attrs) {
+        console.log(attrs);
+        switch(attrs.mode) {
+          case "details":
+          return "/templates/user-details.html";
+          default:
+          return "/templates/user.html";
+        }
+      },
       controller: function() {
-
+        this.fn = function() {
+          console.log(this.user);
+        };
+      },
+      link: function(scope, element, attrs, usersCtrl) {
+        if (usersCtrl !== null) usersCtrl.userElements.push(scope);
       }
     };
   });
